5cc980718ddd1825dab835ef62d07d52d5aeb03d
Ricardokevins <3121416933@qq.com>
2020-12-03 01:27:22 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    while(1)
+    {
+        cout<<"$ "<<endl;
+        string input;
+        cin>>input;
+        cout<<input<<endl;
+    }
+    return 1;
+}
